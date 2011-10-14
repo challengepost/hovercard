46a06d2a718016f8fcc7db5827ec0eb10e563072
Robin Boutros <niuage@gmail.com>
2011-10-14 12:54:30 -0400

    set the size of the hovercard main photo depending on its type

--- app/helpers/hovercard_helper.rb
+++ app/helpers/hovercard_helper.rb
@@ -12,4 +12,10 @@ module HovercardHelper
   def hovercard_url_for(object)
     polymorphic_path [:hovercard, :hovercard, object]
   end
+
+  def size_of photo
+    accepted_sizes = ["70x70", "100x100"]
+    styles = photo.data.styles
+    styles.keys.find { |key| accepted_sizes.include?(styles[key].geometry) } || :small
+  end
 end
