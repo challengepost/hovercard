ecfe40792f33b169fdfebf2156f317afeaf0f41d
Juan C. Müller <jcmuller@gmail.com>
2011-10-19 22:11:15 -0400

    Update the way user challenges, submissions and awards are loaded.

--- app/controllers/hovercard/users_controller.rb
+++ app/controllers/hovercard/users_controller.rb
@@ -3,23 +3,9 @@ module Hovercard
     before_filter :find_user
 
     def hovercard
-      @challenges_count = @user.supported_challenges.published.active.count
-      @submissions_count = Submission.submissions_for(@user).eligible.joins(:challenge).where("challenges.submissions_published = ? and challenges.published_at is not null", true).count
-
-      submission_ids = @user.all_submission_ids
-      submission_ids = ['null'] if submission_ids.blank?
-
-      @awards_count = Submission.includes(:challenge, :prizes).
-        joins(<<-EOF).
-        join `prizes_attributions` on `submissions`.id = `prizes_attributions`.submission_id
-        join `challenges` AS challenges2 on `challenges2`.id  = `submissions`.challenge_id
-      EOF
-      where(<<-EOF).
-        `submissions`.id in (#{submission_ids.join(", ")})
-          and
-        `challenges2`.moderation_stage = "#{Challenge::ModerationStage::FINISHED}"
-      EOF
-      order("`prizes_attributions`.created_at DESC").paginate(:page => 1).total_entries
+      @challenges_count  = @user.visible_challenges.count
+      @submissions_count = @user.visible_submissions.count
+      @awards_count      = @user.visible_awards.count
       super
     end
 
