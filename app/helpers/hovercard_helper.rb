631d95b83642fcfebcc67ad7b52155bf2e4d0137
John Allison <jrallison@gmail.com>
2012-01-04 16:56:30 -0500

    Update challenge hovercard to hide prize and submission counts if 0

--- app/helpers/hovercard_helper.rb
+++ app/helpers/hovercard_helper.rb
@@ -20,4 +20,30 @@ module HovercardHelper
     styles = photo.data.styles
     styles.keys.find { |key| accepted_sizes.include?(styles[key].geometry) } || :small
   end
+
+  def build_challenge_hovercard_data(challenge)
+    data = [
+      { :text => challenge_status(challenge), :class => "state" }
+    ] 
+
+    data.unshift(build_challenge_prize_hovercard_data(challenge)) if challenge.prizes.any? && challenge.total_cash_value > 0 
+    data.unshift(build_challenge_submissions_hovercard_data(challenge)) if challenge.eligible_submissions_count > 0
+    data
+  end
+
+  def build_challenge_prize_hovercard_data(challenge)
+    prize_value = number_to_currency(challenge.prize_value || 0,
+                                     :unit => challenge.currency_unit,
+                                     :format => challenge.currency_format,
+                                     :precision => 0)
+
+    { :text => "<strong>#{prize_value}</strong> in prizes", :class => "awards" }
+  end
+
+  def build_challenge_submissions_hovercard_data(challenge)
+    object = challenge.submission_object
+    count  = challenge.eligible_submissions_count
+
+    { :text => "<strong>#{count}</strong> #{count > 1 ? pluralize(object) : object}</strong> in the gallery", :class => "submissions" }
+  end
 end
